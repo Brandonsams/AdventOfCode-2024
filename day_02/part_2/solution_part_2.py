6880d3067c9500d0d9031578d4ecfce3ec474422
brandonsams <brandonkeithsams@gmail.com>
2024-12-02 19:54:48 -0700

    completed day2 part 2

--- day_02/part_2/solution_part_2.py
+++ day_02/part_2/solution_part_2.py
@@ -12,21 +12,54 @@ def load_input(filename):
             lines.append(line.rstrip())
     return lines
 
+
+def is_safe_report(report):
+    if len(report) <= 1:
+        return True
+
+    diffs = []
+    # loop through pairs of items in report, at i and i+1
+    for i in range(len(report)-1):
+        diff = report[i+1] - report[i]
+        diffs.append(diff)
+
+    is_strictly_increasing = all(diff > 0 for diff in diffs)
+    is_strictly_decreasing = all(diff < 0 for diff in diffs)
+    if not is_strictly_increasing and not is_strictly_decreasing:
+        return False
+
+    is_correctly_sized = all(
+        abs(diff) >= 1 and abs(diff) <= 3 for diff in diffs)
+
+    if not is_correctly_sized:
+        return False
+
+    return True
+
+
+def is_almost_safe_report(report):
+    for j in range(len(report)):
+        sub_report = [x for i, x in enumerate(report) if i != j]
+        if is_safe_report(sub_report):
+            return True
+    return False
+
+
 def solve(filename):
     lines = load_input(filename=filename)
-
-    l_list = []
-    r_list = []
+    reports = []
     for line in lines:
-        l, r = line.split()
-        l_list.append(int(l))
-        r_list.append(int(r))
+        reports.append([int(x) for x in line.split()])
 
-    similarity_score = 0
-    for l in l_list:
-        similarity_score += l * r_list.count(l)
+    safe_report_count = 0
+    for report in reports:
+        if is_safe_report(report=report):
+            safe_report_count += 1
+        else:
+            if is_almost_safe_report(report=report):
+                safe_report_count += 1
 
-    return similarity_score
+    return safe_report_count
 
 
 if __name__ == "__main__":
