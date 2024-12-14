eaa06fb7590715917103000d9cb516981748e466
brandonsams <brandonkeithsams@gmail.com>
2024-12-13 20:19:28 -0700

    day 14 setup

---
+++ day_14/part_2_test.py
@@ -0,0 +1,13 @@
+import pytest # type: ignore
+from day_14.part_2_solution import solve
+from pathlib import Path
+
+root_dir = Path(__file__).parent
+input_file = f"{root_dir}/puzzle_input_example.txt"
+
+expected_answer = 0
+
+@pytest.mark.parametrize("filename, expected", [(input_file, expected_answer)])
+def test_solve(filename, expected):
+    answer = solve(filename=filename)
+    assert answer == expected
